dc4a608ff181496e9a4dfcc4c70f5e5e4eeb689b
Ricardo Zanini <ricardozanini@gmail.com>
2025-09-25 18:22:03 -0400

    bug: fix #9 - Add quarkus-jackson-jq as ExpressionFactory; Add ServiceProviderBuildItem
    Signed-off-by: Ricardo Zanini <ricardozanini@gmail.com>

--- core/deployment/src/main/java/io/quarkiverse/flow/deployment/FlowNativeProcessor.java
+++ core/deployment/src/main/java/io/quarkiverse/flow/deployment/FlowNativeProcessor.java
@@ -1,26 +1,58 @@
 package io.quarkiverse.flow.deployment;
 
+import com.github.f4b6a3.ulid.UlidCreator;
+import com.github.f4b6a3.ulid.UlidFactory;
+
+import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.nativeimage.RuntimeInitializedClassBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ServiceProviderBuildItem;
+import io.serverlessworkflow.impl.events.EventConsumer;
+import io.serverlessworkflow.impl.events.EventPublisher;
+import io.serverlessworkflow.impl.events.InMemoryEvents;
+import io.serverlessworkflow.impl.executors.DefaultTaskExecutorFactory;
+import io.serverlessworkflow.impl.executors.TaskExecutorFactory;
+import io.serverlessworkflow.impl.expressions.ExpressionFactory;
+import io.serverlessworkflow.impl.expressions.jq.JQExpressionFactory;
+import io.serverlessworkflow.impl.jackson.schema.JsonSchemaValidatorFactory;
+import io.serverlessworkflow.impl.schema.SchemaValidatorFactory;
 
-/**
- * see <a href="https://github.com/serverlessworkflow/sdk-java/issues/812">Native-image build fails due to UlidCreator static
- * initialization (Random in image heap)</a>
- */
 final class FlowNativeProcessor {
+
+    /**
+     * see <a href="https://github.com/serverlessworkflow/sdk-java/issues/812">Native-image build fails due to UlidCreator
+     * static
+     * initialization (Random in image heap)</a>
+     */
     @BuildStep
-    RuntimeInitializedClassBuildItem ulidCreatorHolder() {
-        return new RuntimeInitializedClassBuildItem("com.github.f4b6a3.ulid.UlidCreator$MonotonicFactoryHolder");
+    void runtimeInitUlid(BuildProducer<RuntimeInitializedClassBuildItem> producer) {
+        producer.produce(new RuntimeInitializedClassBuildItem(
+                "com.github.f4b6a3.ulid.UlidCreator$MonotonicFactoryHolder"));
+        producer.produce(new RuntimeInitializedClassBuildItem(
+                com.github.f4b6a3.ulid.UlidCreator.class.getName()));
+        producer.produce(new RuntimeInitializedClassBuildItem(
+                com.github.f4b6a3.ulid.UlidFactory.class.getName()));
     }
 
+    /**
+     * Registers the CNCF Java SDK default providers for native compilation.
+     */
     @BuildStep
-    RuntimeInitializedClassBuildItem ulidCreator() {
-        return new RuntimeInitializedClassBuildItem("com.github.f4b6a3.ulid.UlidCreator");
-    }
+    void registerSDKServiceProviders(BuildProducer<ServiceProviderBuildItem> sp) {
 
-    @BuildStep
-    RuntimeInitializedClassBuildItem ulidFactory() {
-        return new RuntimeInitializedClassBuildItem("com.github.f4b6a3.ulid.UlidFactory");
+        // TODO: make all of them @DefaultBeans so users can easily replace
+        // TODO: these providers must be compatible with Quarkus Ecosystem
+
+        sp.produce(new ServiceProviderBuildItem(ExpressionFactory.class.getName(),
+                JQExpressionFactory.class.getName()));
+        sp.produce(new ServiceProviderBuildItem(TaskExecutorFactory.class.getName(),
+                DefaultTaskExecutorFactory.class.getName()));
+        sp.produce(new ServiceProviderBuildItem(SchemaValidatorFactory.class.getName(),
+                JsonSchemaValidatorFactory.class.getName()));
+        sp.produce(new ServiceProviderBuildItem(EventConsumer.class.getName(),
+                InMemoryEvents.class.getName()));
+        sp.produce(new ServiceProviderBuildItem(EventPublisher.class.getName(),
+                InMemoryEvents.class.getName()));
     }
 
 }
