8e7161b66aa2bf664d1f8ff76c292fc3dc495104
Matheus Cruz <matheuscruz.dev@gmail.com>
2025-10-30 19:23:52 -0300

    Disable GenericAgenticTest
    Signed-off-by: Matheus Cruz <matheuscruz.dev@gmail.com>

--- core/integration-tests/src/test/java/io/quarkiverse/flow/it/GenericAgenticTest.java
+++ core/integration-tests/src/test/java/io/quarkiverse/flow/it/GenericAgenticTest.java
@@ -4,12 +4,15 @@ import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 
 import io.quarkus.test.junit.QuarkusTest;
 
+@Disabled("Disabled since on GitHub Actions Ollama stopped working")
 @QuarkusTest
 @DisabledOnOs(OS.WINDOWS)
 class GenericAgenticTest {
