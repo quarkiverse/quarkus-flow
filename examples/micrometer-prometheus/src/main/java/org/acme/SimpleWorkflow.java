8f44888431aa70455569e12e95488bb5d0b11951
Matheus Cruz <matheuscruz.dev@gmail.com>
2026-01-23 17:46:52 -0300

    Update documentation
    Signed-off-by: Matheus Cruz <matheuscruz.dev@gmail.com>

--- examples/micrometer-prometheus/src/main/java/org/acme/SimpleWorkflow.java
+++ examples/micrometer-prometheus/src/main/java/org/acme/SimpleWorkflow.java
@@ -1,10 +1,12 @@
 package org.acme;
 
 import static io.serverlessworkflow.fluent.func.FuncWorkflowBuilder.workflow;
+import static io.serverlessworkflow.fluent.func.dsl.FuncDSL.function;
 import static io.serverlessworkflow.fluent.func.dsl.FuncDSL.set;
 
 import io.quarkiverse.flow.Flow;
 import io.serverlessworkflow.api.types.Workflow;
+import io.serverlessworkflow.fluent.func.dsl.FuncDSL;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
@@ -12,6 +14,7 @@ public class SimpleWorkflow extends Flow {
 
     @Override
     public Workflow descriptor() {
-        return workflow("simple-workflow", "quarkus.flow").tasks(set("{ message: \"Ana Sara\" }")).build();
+        return workflow("simple-workflow", "quarkus.flow")
+                .tasks(set("{ message: \"Ana Sara\" }")).build();
     }
 }
