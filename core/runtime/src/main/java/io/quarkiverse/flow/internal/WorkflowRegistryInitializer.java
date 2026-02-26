600d90aa45edb9e399ce2ad3fd987f34ccb92827
Ricardo Zanini <ricardozanini@gmail.com>
2026-02-26 18:46:32 -0500

    Fix #148 - Refactor the Newsletter Draft example
    Signed-off-by: Ricardo Zanini <ricardozanini@gmail.com>

--- core/runtime/src/main/java/io/quarkiverse/flow/internal/WorkflowRegistryInitializer.java
+++ core/runtime/src/main/java/io/quarkiverse/flow/internal/WorkflowRegistryInitializer.java
@@ -4,15 +4,20 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.quarkus.runtime.StartupEvent;
 
 @ApplicationScoped
 public class WorkflowRegistryInitializer {
 
+    private static final Logger LOG = LoggerFactory.getLogger(WorkflowRegistryInitializer.class);
     @Inject
     WorkflowRegistry registry;
 
     void onStart(@Observes StartupEvent ev) {
+        LOG.debug("Starting Workflow Registry");
         registry.warmUp();
     }
 }
