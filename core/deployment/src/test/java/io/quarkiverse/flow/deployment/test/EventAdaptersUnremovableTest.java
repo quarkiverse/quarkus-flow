d06f37db8fb146178cfed4e37c1691a200d10bab
Matheus André <92062874+matheusandre1@users.noreply.github.com>
2026-03-09 12:55:42 -0300

    feat: EventPublisher and EventConsumer as unremovable bean (#298)

---
+++ core/deployment/src/test/java/io/quarkiverse/flow/deployment/test/EventAdaptersUnremovableTest.java
@@ -0,0 +1,73 @@
+package io.quarkiverse.flow.deployment.test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.util.TypeLiteral;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.cloudevents.CloudEvent;
+import io.quarkus.arc.Arc;
+import io.quarkus.arc.InjectableInstance;
+import io.quarkus.test.QuarkusUnitTest;
+import io.serverlessworkflow.impl.events.AbstractTypeConsumer;
+import io.serverlessworkflow.impl.events.EventConsumer;
+import io.serverlessworkflow.impl.events.EventPublisher;
+
+public class EventAdaptersUnremovableTest {
+
+    @RegisterExtension
+    static final QuarkusUnitTest unitTest = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addClasses(CustomEventPublisher.class, CustomEventConsumer.class));
+
+    @Test
+    void customPublisherAndConsumerAreAvailable() {
+        assertTrue(Arc.container().instance(EventPublisher.class).isAvailable());
+        InjectableInstance<EventConsumer<?, ?>> consumerHandle = Arc.container().select(new TypeLiteral<>() {
+        });
+        assertTrue(consumerHandle.isResolvable());
+    }
+
+    @ApplicationScoped
+    public static class CustomEventPublisher implements EventPublisher {
+        @Override
+        public CompletableFuture<Void> publish(CloudEvent event) {
+            return CompletableFuture.completedFuture(null);
+        }
+
+        @Override
+        public void close() {
+        }
+    }
+
+    @ApplicationScoped
+    public static class CustomEventConsumer extends AbstractTypeConsumer {
+        @Override
+        protected void registerToAll(Consumer<CloudEvent> consumer) {
+        }
+
+        @Override
+        protected void unregisterFromAll() {
+        }
+
+        @Override
+        protected void register(String type, Consumer<CloudEvent> consumer) {
+        }
+
+        @Override
+        protected void unregister(String type) {
+        }
+
+        @Override
+        public void close() {
+        }
+    }
+}
