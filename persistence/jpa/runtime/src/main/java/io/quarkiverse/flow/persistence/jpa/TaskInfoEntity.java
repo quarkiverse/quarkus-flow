cf34974ac6e1288b8e9f706843f9890946dffd79
fjtirado <ftirados@redhat.com>
2026-03-11 15:20:42 +0100

    [Fix #305] Change the primary Id ot the one to many relationship

--- persistence/jpa/runtime/src/main/java/io/quarkiverse/flow/persistence/jpa/TaskInfoEntity.java
+++ persistence/jpa/runtime/src/main/java/io/quarkiverse/flow/persistence/jpa/TaskInfoEntity.java
@@ -3,6 +3,7 @@ package io.quarkiverse.flow.persistence.jpa;
 import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.DiscriminatorType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
@@ -12,6 +13,9 @@ import jakarta.persistence.InheritanceType;
 @DiscriminatorColumn(name = "task_type", discriminatorType = DiscriminatorType.INTEGER)
 public abstract class TaskInfoEntity {
     @Id
+    @GeneratedValue
+    private long id;
+
     private String jsonPointer;
 
     public TaskInfoEntity() {
