899d93cc6a9bbc186fe5023755c6b14553ccd9df
Dominik Hanak <dhanak@redhat.com>
2026-04-12 19:30:50 +0200

    Add ForEachWorkflow with tests

---
+++ docs/modules/ROOT/examples/org/acme/ForEachWorkflow.java
@@ -0,0 +1,28 @@
+package org.acme;
+
+import java.util.List;
+import java.util.Map;
+
+import io.quarkiverse.flow.Flow;
+import io.serverlessworkflow.api.types.Workflow;
+import io.serverlessworkflow.fluent.func.FuncWorkflowBuilder;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import static io.serverlessworkflow.fluent.func.dsl.FuncDSL.*;
+
+@ApplicationScoped
+public class ForEachWorkflow extends Flow {
+    @Override
+    public Workflow descriptor() {
+        return FuncWorkflowBuilder.workflow("foreach-workflow")
+                .tasks(
+                        // OrdersPayload::orders is not working, TODO: Check why, probably the Record class I use?
+                        forEach((Map<String, Object> state) -> (List<?>) state.get("orders"),
+                                tasks(
+                                    post("", "http://localhost:8089/process-order")
+
+                        ))
+                )
+                .build();
+    }
+}
