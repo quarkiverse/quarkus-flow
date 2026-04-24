e8ee9248e9819f55b5214c60884571d5258a75dd
Dominik Hanak <dhanak@redhat.com>
2026-04-24 09:32:35 +0200

    Fix formatting of ContextWorkflow

--- docs/modules/ROOT/examples/org/acme/ContextWorkflow.java
+++ docs/modules/ROOT/examples/org/acme/ContextWorkflow.java
@@ -1,14 +1,12 @@
 package org.acme;
 
-import static io.serverlessworkflow.fluent.func.dsl.FuncDSL.set;
 import static io.serverlessworkflow.fluent.func.dsl.FuncDSL.withContext;
-import static io.serverlessworkflow.fluent.spec.WorkflowBuilder.workflow;
 
-import io.serverlessworkflow.fluent.func.FuncWorkflowBuilder;
 import jakarta.enterprise.context.ApplicationScoped;
 
 import io.quarkiverse.flow.Flow;
 import io.serverlessworkflow.api.types.Workflow;
+import io.serverlessworkflow.fluent.func.FuncWorkflowBuilder;
 
 @ApplicationScoped
 public class ContextWorkflow extends Flow {
@@ -16,11 +14,11 @@ public class ContextWorkflow extends Flow {
     public Workflow descriptor() {
         return FuncWorkflowBuilder.workflow("context-aware")
                 .tasks(
-                    withContext((p, c) -> {
-                        System.out.println("Instance ID: " + c.instanceData().id());
-                        return "Processed " + p;
-                    }, String.class)
-                            //.exportAsTaskOutput()
+                        withContext((p, c) -> {
+                            System.out.println("Instance ID: " + c.instanceData().id());
+                            return "Processed " + p;
+                        }, String.class)
+                //.exportAsTaskOutput()
                 )
                 .build();
     }
