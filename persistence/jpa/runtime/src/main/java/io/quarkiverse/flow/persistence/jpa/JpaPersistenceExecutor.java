30c6d4a94fa5633278de2110126c189f4040288a
Matheus Cruz <matheuscruz.dev@gmail.com>
2026-06-04 00:43:24 -0300

    Remove @Transactional from JpaPersistenceExecutor#execute methods
    Signed-off-by: Matheus Cruz <matheuscruz.dev@gmail.com>

--- persistence/jpa/runtime/src/main/java/io/quarkiverse/flow/persistence/jpa/JpaPersistenceExecutor.java
+++ persistence/jpa/runtime/src/main/java/io/quarkiverse/flow/persistence/jpa/JpaPersistenceExecutor.java
@@ -7,7 +7,6 @@ import java.util.function.Supplier;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.transaction.Transactional;
 
 import org.eclipse.microprofile.context.ManagedExecutor;
 
@@ -21,13 +20,11 @@ public class JpaPersistenceExecutor extends AbstractAsyncPersistenceExecutor {
     ManagedExecutor service;
 
     @Override
-    @Transactional
     public <T> CompletableFuture<T> execute(Supplier<T> runnable, WorkflowDefinitionData definition) {
         return super.execute(runnable, definition);
     }
 
     @Override
-    @Transactional
     public CompletableFuture<Void> execute(Runnable runnable, WorkflowDefinitionData definition) {
         return super.execute(runnable, definition);
     }
