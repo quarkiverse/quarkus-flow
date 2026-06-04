c3b77f88627f6e66403c9ee278f9d0366a36b3ce
fjtirado <ftirados@ibm.com>
2026-06-04 12:23:33 +0200

    [Fix #600] Use refined trnsactional approach
    Signed-off-by: fjtirado <ftirados@ibm.com>

--- persistence/jpa/runtime/src/main/java/io/quarkiverse/flow/persistence/jpa/JpaPersistenceHandlerProducer.java
+++ persistence/jpa/runtime/src/main/java/io/quarkiverse/flow/persistence/jpa/JpaPersistenceHandlerProducer.java
@@ -1,12 +1,22 @@
 package io.quarkiverse.flow.persistence.jpa;
 
+import java.util.Collection;
+import java.util.Map;
+import java.util.function.Function;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
 
-import io.serverlessworkflow.impl.persistence.PersistenceAllStrategyCorrelationInfoFactories;
+import io.cloudevents.CloudEvent;
+import io.quarkus.narayana.jta.QuarkusTransaction;
+import io.serverlessworkflow.impl.WorkflowDefinition;
+import io.serverlessworkflow.impl.events.EventRegistrationBuilder;
+import io.serverlessworkflow.impl.persistence.CorrelationOperations;
+import io.serverlessworkflow.impl.persistence.OperationAllStrategyCorrelationInfo;
 import io.serverlessworkflow.impl.persistence.PersistenceExecutor;
 import io.serverlessworkflow.impl.persistence.PersistenceInstanceHandlers;
 import io.serverlessworkflow.impl.persistence.PersistenceInstanceOperations;
+import io.serverlessworkflow.impl.scheduler.AllStrategyCorrelationInfo;
 import io.serverlessworkflow.impl.scheduler.AllStrategyCorrelationInfoFactory;
 
 public class JpaPersistenceHandlerProducer {
@@ -21,6 +31,36 @@ public class JpaPersistenceHandlerProducer {
     @Produces
     AllStrategyCorrelationInfoFactory correlationFactory(PersistenceExecutor executor,
             PersistenceInstanceOperations operations) {
-        return PersistenceAllStrategyCorrelationInfoFactories.from(executor, operations);
+        return new JpaCorrelationStrategyFactory(executor, operations);
+    }
+
+    private static class JpaCorrelationStrategyFactory implements AllStrategyCorrelationInfoFactory {
+
+        private final PersistenceExecutor executor;
+        private final PersistenceInstanceOperations operations;
+
+        public JpaCorrelationStrategyFactory(PersistenceExecutor executor, PersistenceInstanceOperations operations) {
+            this.executor = executor;
+            this.operations = operations;
+        }
+
+        @Override
+        public AllStrategyCorrelationInfo apply(WorkflowDefinition def) {
+            return new JpaCorrelationStrategy(def, executor, operations);
+        }
+    }
+
+    private static class JpaCorrelationStrategy extends OperationAllStrategyCorrelationInfo {
+
+        public JpaCorrelationStrategy(WorkflowDefinition definition, PersistenceExecutor executor,
+                PersistenceInstanceOperations operations) {
+            super(definition, executor, operations);
+        }
+
+        @Override
+        protected Collection<Map<EventRegistrationBuilder, CloudEvent>> doTransaction(
+                Function<CorrelationOperations, Collection<Map<EventRegistrationBuilder, CloudEvent>>> function) {
+            return QuarkusTransaction.requiringNew().call(() -> super.doTransaction(function));
+        }
     }
 }
