eb68a1abcdafa1bc06902d83c09606ed35fe5648
fjtirado <ftirados@ibm.com>
2026-06-05 12:00:05 +0200

    [Fix #600] Adding IT test
    Signed-off-by: fjtirado <ftirados@ibm.com>

---
+++ persistence/jpa/integration-tests/src/main/java/io/quarkiverse/flow/persistence/jpa/test/HelloWorkflow.java
@@ -0,0 +1,22 @@
+package io.quarkiverse.flow.persistence.jpa.test;
+
+import static io.serverlessworkflow.fluent.func.dsl.FuncDSL.set;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+import io.quarkiverse.flow.Flow;
+import io.serverlessworkflow.api.types.Workflow;
+import io.serverlessworkflow.fluent.func.FuncWorkflowBuilder;
+
+@ApplicationScoped
+public class HelloWorkflow extends Flow {
+
+    @Override
+    public Workflow descriptor() {
+        return FuncWorkflowBuilder.workflow("hello")
+                .tasks(set("{ message: \"hello world!\" }"))
+                .build();
+
+    }
+
+}
---
+++ persistence/jpa/integration-tests/src/main/java/io/quarkiverse/flow/persistence/jpa/test/Message.java
@@ -0,0 +1,5 @@
+package io.quarkiverse.flow.persistence.jpa.test;
+
+public record Message(String message) {
+
+}
