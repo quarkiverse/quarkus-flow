b9384a2eb3ca687ad0d4d0df30ceba98398ac50b
Francisco Javier Tirado Sarti <ftirados@ibm.com>
2026-06-11 16:21:26 +0200

    [Fix #462] Adding quartz base scheduler (#610)
    * [Fix #462] Adding quartz base scheduler
    
    Signed-off-by: fjtirado <ftirados@ibm.com>
    
    * [Fix #462] Ricardos comments
    
    Signed-off-by: fjtirado <ftirados@ibm.com>
    
    * [Fix #462] Updating docs
    
    Signed-off-by: fjtirado <ftirados@ibm.com>
    
    * [Fix #462] Updating docs
    
    Signed-off-by: fjtirado <ftirados@ibm.com>
    
    ---------
    
    Signed-off-by: fjtirado <ftirados@ibm.com>

---
+++ scheduler/quartz/runtime/src/main/java/io/quarkiverse/flow/quartz/FlowQuartzApplicationBuilderCustomizer.java
@@ -0,0 +1,21 @@
+package io.quarkiverse.flow.quartz;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import io.quarkiverse.flow.recorders.WorkflowApplicationBuilderCustomizer;
+import io.quarkus.arc.Unremovable;
+import io.serverlessworkflow.impl.WorkflowApplication;
+
+@ApplicationScoped
+@Unremovable
+public class FlowQuartzApplicationBuilderCustomizer implements WorkflowApplicationBuilderCustomizer {
+
+    @Inject
+    FlowQuartz scheduler;
+
+    @Override
+    public void customize(WorkflowApplication.Builder builder) {
+        builder.withScheduler(scheduler);
+    }
+}
