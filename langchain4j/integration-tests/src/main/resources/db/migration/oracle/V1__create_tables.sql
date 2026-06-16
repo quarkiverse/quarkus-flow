4e297e36445906e009c64c52c5212df7bdbe06c1
Matheus Cruz <56329339+mcruzdev@users.noreply.github.com>
2026-06-16 18:41:36 -0300

    Add flyway migration db scripts (#634)
    * Add db migrations
    
    Signed-off-by: Matheus Cruz <matheuscruz.dev@gmail.com>
    
    * Fix CI for h2 database
    
    Signed-off-by: Matheus Cruz <matheuscruz.dev@gmail.com>
    
    * Rename Durable to Persistent and skip langchain4j/integration-tests on build-it.yml
    
    Signed-off-by: Matheus Cruz <matheuscruz.dev@gmail.com>
    
    ---------
    
    Signed-off-by: Matheus Cruz <matheuscruz.dev@gmail.com>

---
+++ langchain4j/integration-tests/src/main/resources/db/migration/oracle/V1__create_tables.sql
@@ -0,0 +1,50 @@
+CREATE TABLE cloud_event_entity
+(
+    id                VARCHAR2(255) NOT NULL,
+    reg_id            VARCHAR2(255) NOT NULL,
+    source            VARCHAR2(255) NOT NULL,
+    type              VARCHAR2(255) NOT NULL,
+    subject           VARCHAR2(255),
+    data_content_type VARCHAR2(255),
+    data_schema       VARCHAR2(255),
+    time              TIMESTAMP(6) WITH TIME ZONE,
+    data              BLOB,
+    extensions        BLOB,
+    processed_flag    NUMBER(1, 0) DEFAULT 0,
+    version           NUMBER(3, 0)  NOT NULL CHECK (version BETWEEN 0 AND 1),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE process_instance_entity
+(
+    application_id     VARCHAR2(255)               NOT NULL,
+    instance_id        VARCHAR2(255)               NOT NULL,
+    workflow_name      VARCHAR2(255)               NOT NULL,
+    workflow_namespace VARCHAR2(255)               NOT NULL,
+    workflow_version   VARCHAR2(255)               NOT NULL,
+    started_at         TIMESTAMP(6) WITH TIME ZONE NOT NULL,
+    status             NUMBER(3, 0) CHECK (status BETWEEN 0 AND 6),
+    input              BLOB,
+    PRIMARY KEY (application_id, instance_id)
+);
+
+CREATE TABLE task_info_entity
+(
+    application_id      VARCHAR2(255) NOT NULL,
+    process_instance_id VARCHAR2(255) NOT NULL,
+    json_pointer        VARCHAR2(255) NOT NULL,
+    iteration           NUMBER(10, 0) NOT NULL,
+    task_type           NUMBER(10, 0) NOT NULL CHECK (task_type IN (1, 2)),
+    is_end_node         NUMBER(1, 0),
+    retry_attempt       NUMBER(5, 0),
+    instant             TIMESTAMP(6) WITH TIME ZONE,
+    next_position       VARCHAR2(255),
+    context             BLOB,
+    model               BLOB,
+    PRIMARY KEY (iteration, application_id, json_pointer, process_instance_id),
+    CHECK (task_type <> 1 OR (is_end_node IS NOT NULL)),
+    CHECK (task_type <> 2 OR (retry_attempt IS NOT NULL)),
+    CONSTRAINT fk_task_process_instance
+        FOREIGN KEY (application_id, process_instance_id)
+            REFERENCES process_instance_entity (application_id, instance_id)
+);
---
+++ langchain4j/integration-tests/src/main/resources/db/migration/postgresql/V1__create_tables.sql
@@ -0,0 +1,50 @@
+CREATE TABLE cloud_event_entity
+(
+    id                VARCHAR(255) NOT NULL,
+    reg_id            VARCHAR(255) NOT NULL,
+    source            VARCHAR(255) NOT NULL,
+    type              VARCHAR(255) NOT NULL,
+    subject           VARCHAR(255),
+    data_content_type VARCHAR(255),
+    data_schema       VARCHAR(255),
+    time              TIMESTAMP(6) WITH TIME ZONE,
+    data              BYTEA,
+    extensions        BYTEA,
+    processed_flag    BOOLEAN DEFAULT FALSE,
+    version           SMALLINT     NOT NULL CHECK (version BETWEEN 0 AND 1),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE process_instance_entity
+(
+    application_id     VARCHAR(255)                NOT NULL,
+    instance_id        VARCHAR(255)                NOT NULL,
+    workflow_name      VARCHAR(255)                NOT NULL,
+    workflow_namespace VARCHAR(255)                NOT NULL,
+    workflow_version   VARCHAR(255)                NOT NULL,
+    started_at         TIMESTAMP(6) WITH TIME ZONE NOT NULL,
+    status             SMALLINT CHECK (status BETWEEN 0 AND 6),
+    input              BYTEA,
+    PRIMARY KEY (application_id, instance_id)
+);
+
+CREATE TABLE task_info_entity
+(
+    application_id      VARCHAR(255) NOT NULL,
+    process_instance_id VARCHAR(255) NOT NULL,
+    json_pointer        VARCHAR(255) NOT NULL,
+    iteration           INTEGER      NOT NULL,
+    task_type           INTEGER      NOT NULL CHECK (task_type IN (1, 2)),
+    is_end_node         BOOLEAN,
+    retry_attempt       SMALLINT,
+    instant             TIMESTAMP(6) WITH TIME ZONE,
+    next_position       VARCHAR(255),
+    context             BYTEA,
+    model               BYTEA,
+    PRIMARY KEY (iteration, application_id, json_pointer, process_instance_id),
+    CHECK (task_type <> 1 OR (is_end_node IS NOT NULL)),
+    CHECK (task_type <> 2 OR (retry_attempt IS NOT NULL)),
+    CONSTRAINT fk_task_process_instance
+        FOREIGN KEY (application_id, process_instance_id)
+            REFERENCES process_instance_entity (application_id, instance_id)
+);
