5e73ebdc5cac11297984c0dff24763bc5b024c09
Matheus Cruz <matheuscruz.dev@gmail.com>
2026-06-17 19:40:32 -0300

    Format persistence/jpa/runtime/src/main/java/io/quarkiverse/flow/persistence/jpa/JpaPersistenceExecutor.java class
    Signed-off-by: Matheus Cruz <matheuscruz.dev@gmail.com>

--- persistence/jpa/runtime/src/main/java/io/quarkiverse/flow/persistence/jpa/JpaPersistenceExecutor.java
+++ persistence/jpa/runtime/src/main/java/io/quarkiverse/flow/persistence/jpa/JpaPersistenceExecutor.java
@@ -3,14 +3,14 @@ package io.quarkiverse.flow.persistence.jpa;
 import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 
-import io.smallrye.context.api.ManagedExecutorConfig;
 import jakarta.enterprise.context.ApplicationScoped;
-
 import jakarta.inject.Inject;
+
 import org.eclipse.microprofile.context.ManagedExecutor;
 import org.eclipse.microprofile.context.ThreadContext;
 
 import io.serverlessworkflow.impl.persistence.AbstractAsyncPersistenceExecutor;
+import io.smallrye.context.api.ManagedExecutorConfig;
 
 @ApplicationScoped
 public class JpaPersistenceExecutor extends AbstractAsyncPersistenceExecutor {
