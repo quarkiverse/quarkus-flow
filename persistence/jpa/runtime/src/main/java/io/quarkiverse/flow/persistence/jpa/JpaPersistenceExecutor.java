66965931e66f5474b60328215a69d4e63eb81faa
Matheus Cruz <matheuscruz.dev@gmail.com>
2026-06-17 18:09:03 -0300

    Make the async persistence executor run with the transaction context cleared
    Signed-off-by: Matheus Cruz <matheuscruz.dev@gmail.com>

--- persistence/jpa/runtime/src/main/java/io/quarkiverse/flow/persistence/jpa/JpaPersistenceExecutor.java
+++ persistence/jpa/runtime/src/main/java/io/quarkiverse/flow/persistence/jpa/JpaPersistenceExecutor.java
@@ -3,10 +3,12 @@ package io.quarkiverse.flow.persistence.jpa;
 import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 
+import io.smallrye.context.api.ManagedExecutorConfig;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
 
+import jakarta.inject.Inject;
 import org.eclipse.microprofile.context.ManagedExecutor;
+import org.eclipse.microprofile.context.ThreadContext;
 
 import io.serverlessworkflow.impl.persistence.AbstractAsyncPersistenceExecutor;
 
@@ -14,6 +16,8 @@ import io.serverlessworkflow.impl.persistence.AbstractAsyncPersistenceExecutor;
 public class JpaPersistenceExecutor extends AbstractAsyncPersistenceExecutor {
 
     @Inject
+    // See https://github.com/quarkiverse/quarkus-flow/issues/652
+    @ManagedExecutorConfig(cleared = ThreadContext.TRANSACTION)
     ManagedExecutor service;
 
     @Override
