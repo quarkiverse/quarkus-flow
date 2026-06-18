f8ab74f77be51341669bfa0864b1398a2998998b
Matheus Cruz <56329339+mcruzdev@users.noreply.github.com>
2026-06-18 06:37:08 -0300

    Close MVStorePersistenceStore (#657)
    * Close MVStorePersistenceStore
    
    Signed-off-by: Matheus Cruz <matheuscruz.dev@gmail.com>
    
    * Add tests to MVStoreProducer
    
    Signed-off-by: Matheus Cruz <matheuscruz.dev@gmail.com>
    
    ---------
    
    Signed-off-by: Matheus Cruz <matheuscruz.dev@gmail.com>

--- persistence/mvstore/runtime/src/main/java/io/quarkiverse/flow/persistence/mvstore/MVStoreProducer.java
+++ persistence/mvstore/runtime/src/main/java/io/quarkiverse/flow/persistence/mvstore/MVStoreProducer.java
@@ -1,7 +1,11 @@
 package io.quarkiverse.flow.persistence.mvstore;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Disposes;
 import jakarta.enterprise.inject.Produces;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.serverlessworkflow.impl.marshaller.WorkflowBufferFactory;
 import io.serverlessworkflow.impl.persistence.DefaultPersistenceInstanceHandlers;
@@ -12,11 +16,27 @@ import io.serverlessworkflow.impl.persistence.mvstore.MVStorePersistenceStore;
 @ApplicationScoped
 public class MVStoreProducer {
 
+    static final Logger LOG = LoggerFactory.getLogger(MVStoreProducer.class);
+
     @Produces
     @ApplicationScoped
-    PersistenceInstanceHandlers mvStoreHandlers(MVStoreConfig config, WorkflowBufferFactory factory,
+    MVStorePersistenceStore persistenceStore(MVStoreConfig config, WorkflowBufferFactory factory) {
+        return new MVStorePersistenceStore(config.dbPath(), factory);
+    }
+
+    @Produces
+    @ApplicationScoped
+    PersistenceInstanceHandlers mvStoreHandlers(MVStorePersistenceStore store,
             PersistenceExecutor executor) {
-        return DefaultPersistenceInstanceHandlers.builder(new MVStorePersistenceStore(config.dbPath(), factory))
+        return DefaultPersistenceInstanceHandlers.builder(store)
                 .withPersistenceExecutor(executor).build();
+    }
+
+    public void close(@Disposes MVStorePersistenceStore persistenceStore) {
+        try {
+            persistenceStore.close();
+        } catch (Exception e) {
+            LOG.warn("Error while closing MVStorePersistenceStore", e);
+        }
     }
 }
